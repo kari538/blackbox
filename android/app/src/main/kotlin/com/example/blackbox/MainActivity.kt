a9bbfd7aeaa26b2db35ce9e1b0c8782b75204c36
kari538 <karolinahagegard@gmail.com>
2024-02-04 16:18:30 +0100

    c19 Removing app/src/java and kotlin from git, After merging w new_build_on_Mac

--- android/app/src/main/kotlin/com/example/blackbox/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.blackbox
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/karolinadart/blackbox/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.karolinadart.blackbox
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
